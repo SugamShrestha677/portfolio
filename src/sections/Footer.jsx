f8ac10ac1322a4fabe0dd786c7cfe01472ffa575
sugamshrestha677 <xresthasugam@gmail.com>
2025-09-11 09:53:47 +0545

    URl's are updated in linkedin and github in footer section

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -8,13 +8,13 @@ const Footer = () => {
     <footer>
         <div className='min-h-[100px] flex flex-col justify-center gap-6 text-center text-lg text-gray-300 font-mono mb-10'>
             <div className='flex gap-10 p-4 justify-center text-white'>
-                <a href="" className='border p-2 text-2xl rounded hover:-translate-y-2 transition-all bg-blue-700 text-white' target='_blank'>
+                <a href="https://www.linkedin.com/in/sugam-shrestha-21a1a1325/" className='border p-2 text-2xl rounded hover:-translate-y-2 transition-all bg-blue-700 text-white' target='_blank'>
                     <SlSocialLinkedin/>
                 </a>
-                <a href="" className='border p-2 text-2xl rounded hover:-translate-y-2 transition-all bg-black text-white' target='_blank'>
+                <a href="https://github.com/SugamShrestha677" className='border p-2 text-2xl rounded hover:-translate-y-2 transition-all bg-black text-white' target='_blank'>
                     <FaSquareGithub/>
                 </a>
-                <a href="" className='border p-2 text-2xl rounded hover:-translate-y-2 transition-all bg-gray-800 text-white' target='_blank'>
+                <a href="https://www.instagram.com/" className='border p-2 text-2xl rounded hover:-translate-y-2 transition-all bg-gray-800 text-white' target='_blank'>
                     <FaInstagramSquare/>
                 </a>
             </div>
